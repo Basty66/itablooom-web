6018efdeb037380be867950963b1e7ba3c22f4cb
Basty66 <cri.cerdaa@duocuc.cl>
2026-08-13 22:45:47 -0400

    Fix TypeScript import extensions for Vercel

--- api/calendar/index.ts
+++ api/calendar/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { createCalendarEvent } from './_lib';
+import { createCalendarEvent } from './_lib.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
